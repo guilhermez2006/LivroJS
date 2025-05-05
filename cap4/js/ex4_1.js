6b23443b23a61c3151e4a00730d215ace0444a5e
Guilherme Lima <guilhermefk123@gmail.com>
2025-05-05 19:38:18 -0300

    Ajustes finais no ex4_1

--- cap4/js/ex4_1.js
+++ cap4/js/ex4_1.js
@@ -5,6 +5,12 @@ function mostrarTabuada() {
     var outTabuada = document.getElementById("outTabuada");
     let tabuada = "";
 
+    if (inNumero == 0 || isNaN(inNumero)) {
+        alert("Informe um número válido...");
+        inNumero.focus();
+        return;
+    }
+
     for (let contador = 1; contador <= 10; contador++) {
         tabuada += `${inNumero} x ${contador} = ${inNumero * contador}\n`;
     }
